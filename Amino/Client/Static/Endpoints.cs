7e631ec82f8efd9deb8ec943c32cb386288e0d73
ShammTzy <116624277+ShammTzy@users.noreply.github.com>
2023-02-07 17:06:39 +0800

    update

---
+++ Amino/Client/Static/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amino.Client.Static
+{
+    public class Endpoints
+    {
+        public static readonly string Prefix = "api";
+
+        public static readonly string GamesEndPoint = $"{Prefix}/games";
+        public static readonly string OrdersEndPoint = $"{Prefix}/orders";
+        public static readonly string PaymentsEndPoint = $"{Prefix}/payments";
+        public static readonly string PlatformsEndPoint = $"{Prefix}/platforms";
+        public static readonly string CustomersEndPoint = $"{Prefix}/customers";
+    }
+}
